0703b1b0588bcfee1b44dee011f4edac779329c3
hanjala <shihab2305341402@diu.edu.bd>
2025-07-18 21:43:00 +0600

    first commit

---
+++ introduction.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Sample data
+arr = [1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9]
+
+# Create a histogram plot
+sns.displot(arr)
+
+# Show the plot window
+plt.show()
