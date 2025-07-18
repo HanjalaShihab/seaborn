1bd5dc1bf94ea06c96a54238d043cb735dcb8f9e
hanjala <shihab2305341402@diu.edu.bd>
2025-07-18 22:00:47 +0600

    plotting a line graph

--- introduction.py
+++ introduction.py
@@ -9,3 +9,8 @@ data = pd.read_csv(fifa_filepath, index_col="Date", parse_dates= True)
 print(data.info())
 print("--------------------")
 print(data.head())
+
+
+plt.figure(figsize=(16,6))
+sns.lineplot(data=data)
+plt.show()
