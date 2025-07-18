6e9a5b6cb81df055946f5040c8e2acffa93bf04d
hanjala <shihab2305341402@diu.edu.bd>
2025-07-18 21:56:59 +0600

    file path read as date

--- introduction.py
+++ introduction.py
@@ -1,2 +1,11 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+
+fifa_filepath = "fifa.csv"
+
+data = pd.read_csv(fifa_filepath, index_col="Date", parse_dates= True)
+
+print(data.info())
+print("--------------------")
+print(data.head())
