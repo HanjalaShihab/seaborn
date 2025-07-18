e666feabf8b634dd9eeae219ca3820ae540f26da
hanjala <shihab2305341402@diu.edu.bd>
2025-07-18 23:39:55 +0600

    practice

--- spotify/another.py
+++ spotify/another.py
@@ -2,10 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+                           #loading the data
 file_path = "spotify/spotify.csv"
 
 data = pd.read_csv(file_path, index_col="Date", parse_dates = True)
 
+
+                           #examine the data
 print(data.columns)
 print("-------------------")
 print(data.head())
@@ -13,3 +17,10 @@ print("-------------------")
 print(data.tail())
 print("-------------------")
 
+                           #plotting the data
+plt.figure(figsize=(16,6))
+
+plt.title("Daily global streams of popular songs in 2017-2018")
+
+sns.lineplot(data= data)
+plt.show()
