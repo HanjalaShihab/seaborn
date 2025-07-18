f5afe68e90371abbd12583968258f2493403291c
hanjala <shihab2305341402@diu.edu.bd>
2025-07-18 23:23:01 +0600

    practice with spotify dataset

---
+++ spotify/another.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path = "spotify/spotify.csv"
+
+data = pd.read_csv(file_path, index_col="Date", parse_dates = True)
+
+print(data.columns)
