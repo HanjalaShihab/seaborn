76bbf13e853e5e0008ff0c1b1a85a8caf12edbc4
hanjala <shihab2305341402@diu.edu.bd>
2025-07-18 23:24:39 +0600

    practice

--- spotify/another.py
+++ spotify/another.py
@@ -7,3 +7,9 @@ file_path = "spotify/spotify.csv"
 data = pd.read_csv(file_path, index_col="Date", parse_dates = True)
 
 print(data.columns)
+print("-------------------")
+print(data.head())
+print("-------------------")
+print(data.tail())
+print("-------------------")
+
