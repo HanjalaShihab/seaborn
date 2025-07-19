3a586076de515b796b00511eb6c7199b56796f5a
hanjala <shihab2305341402@diu.edu.bd>
2025-07-19 20:13:31 +0600

    bar chart plotting

--- Bar chart/bar-chart.py
+++ Bar chart/bar-chart.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file_path = "Bar chart/fligh_delays.csv"
+file_path = "Bar chart/flight_delays.csv"
 
 data = pd.read_csv(file_path, index_col= "Month")
 
+              #examine data:
+print(data)
 
+              #plotting bar chart:
+plt.figure(figsize=(10, 6))
+plt.title("Average arrival delay for spirit airlines flights, by month")
