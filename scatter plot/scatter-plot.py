70334130bd8172d66e11d418fcb5da8176f3ec78
hanjala <shihab2305341402@diu.edu.bd>
2025-07-20 21:57:48 +0600

    scatter plot

--- scatter plot/scatter-plot.py
+++ scatter plot/scatter-plot.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 file_path = "scatter plot/insurance.csv"
 insurance = pd.read_csv(file_path)
+
+x = insurance.head()
+print(x)   #checking if the data is loaded properly
+
+sns.scatterplot(x = insurance['bmi'], y = insurance['charges'])
+
+plt.show()
