677a352c6f7b5038a0ce8d1f2b00a460c5a348bf
hanjala <shihab2305341402@diu.edu.bd>
2025-07-20 21:06:11 +0600

    bar chart plotting

--- Bar chart/bar-chart.py
+++ Bar chart/bar-chart.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 file_path = "Bar chart/flight_delays.csv"
@@ -11,3 +12,8 @@ print(data)
               #plotting bar chart:
 plt.figure(figsize=(10, 6))
 plt.title("Average arrival delay for spirit airlines flights, by month")
+
+sns.barplot(x = data.index, y = data['NK'])
+
+plt.ylabel("Arrival delay (in minutes)")
+plt.show()
