50136009b005fa5d05e6580bb473fdcff93aa694
hanjala <shihab2305341402@diu.edu.bd>
2025-07-20 23:56:34 +0600

    lmplot for color coded scatter plot

--- scatter plot/lmplot-color-coded.py
+++ scatter plot/lmplot-color-coded.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+#This scatter plot shows that while nonsmokers to tend to pay slightly more with increasing BMI, smokers pay MUCH more.
+
+#To further emphasize this fact, we can use the sns.lmplot command to add two regression lines, corresponding to smokers and nonsmokers.
+#(You'll notice that the regression line for smokers has a much steeper slope, relative to the line for nonsmokers!)
+
 
 file_path = "scatter plot/insurance.csv"
 insurance = pd.read_csv(file_path)
