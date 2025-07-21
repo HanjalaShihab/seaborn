6429e9b7628660ad03f8d4d0ecd667d0ee8ad246
hanjala <shihab2305341402@diu.edu.bd>
2025-07-21 19:52:57 +0600

    histogram and renamed columns to a new csv file

--- Distributions/histogram.py
+++ Distributions/histogram.py
@@ -3,7 +3,23 @@ import seaborn as sns
 import pandas as pd
 
 
+#loading the data:
 file_path = "Distributions/iris.csv"
 iris = pd.read_csv(file_path, index_col="Id")
 
+#renaming the columns for better access:
+iris = iris.rename(columns= {
+    'Sepal Length (cm)': 'SL',
+    'Sepal Width (cm)': 'SW',
+    'Petal Length (cm)': 'PL',
+    'Petal Width (cm)': 'PW'
+})
 print(iris.head())
+
+#saving the coulmn names to a new csv file:
+iris.to_csv("Distributions/iris_renamed.csv")
+
+#plotting the histogram:
+sns.histplot(iris['PL'])
+
+plt.show()
